01b0aa88209a4f3ca0414a0f11699f96aec1b5f1
Luis del Amo <luisdelamo@gmail.com>
2025-11-15 14:00:52 +0100

    feat: change vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		environment: 'node',
-		globals: true,
-		include: ['src/tests/**/*.test.ts'],
+		include: ['src/**/*.{test,spec}.ts'],
 	},
 });
